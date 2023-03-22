d7430affcd219cb23211a72a15fcd0e8846de560
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-03-22 13:56:40 +0100

    add MILESTONE 1

--- asset/js/main.js
+++ asset/js/main.js
@@ -2,45 +2,50 @@
 nome
 ruolo
 foto
-MILESTONE 0:
-Creare l’array di oggetti con le informazioni fornite.
-MILESTONE 1:
-Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+
 MILESTONE 2:
 Stampare le stesse informazioni su DOM sottoforma di stringhe
 BONUS 1:
 Trasformare la stringa foto in una immagine effettiva
 BONUS 2:
 Organizzare i singoli membri in card/schede */
+//MILESTONE 0:
+//Creare l’array di oggetti con le informazioni fornite.
 const teams = [
     {
         nome:"Wayne Barnett",
         ruolo:"Founder & CEO",
-        foto:wayne-barnett-founder-ceo.jpg
+        foto:"wayne-barnett-founder-ceo.jpg"
     },
     {
         nome:"Angela Caroll",
         ruolo:"Chief Editor",
-        foto:angela-caroll-chief-editor.jpg
+        foto:"angela-caroll-chief-editor.jpg"
     },
     {
         nome:"Walter Gordon",
         ruolo:"Office Manager",
-        foto:walter-gordon-office-manager.jpg
+        foto:"walter-gordon-office-manager.jpg"
     },
     {
         nome:"Angela Lopez",
         ruolo:"Social Media Manager",
-        foto:angela-lopez-social-media-manager.jpg
+        foto:"angela-lopez-social-media-manager.jpg"
     },
     {
         nome:"Scott Estrada",
         ruolo:"Developer",
-        foto:scott-estrada-developer.jpg
+        foto:"scott-estrada-developer.jpg"
     },
     {
         nome:"Barbara Ramos",
         ruolo:"Graphic Designer",
-        foto:barbara-ramos-graphic-designer.jpg
+        foto:"barbara-ramos-graphic-designer.jpg"
     },
 ]
+//MILESTONE 1:
+//Stampare su console, per ogni membro del team, le informazioni di nome, ruolo e la stringa della foto
+for (let i = 0; i < teams.length; i++) {
+    const membre = teams[i];
+    console.log(membre)
+}
