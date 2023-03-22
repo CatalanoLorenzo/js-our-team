ee4ca994bad3b51a032f5653d0cd682b219b6087
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-03-22 14:09:24 +0100

    add Bonus 1 and 2

--- asset/js/main.js
+++ asset/js/main.js
@@ -3,12 +3,9 @@ nome
 ruolo
 foto
 
-MILESTONE 2:
-Stampare le stesse informazioni su DOM sottoforma di stringhe
-BONUS 1:
-Trasformare la stringa foto in una immagine effettiva
-BONUS 2:
-Organizzare i singoli membri in card/schede */
+
+
+*/
 //MILESTONE 0:
 //Creare l’array di oggetti con le informazioni fornite.
 const teams = [
@@ -49,11 +46,34 @@ const rowEl = document.querySelector(".row")
 for (let i = 0; i < teams.length; i++) {
     const membre = teams[i];
     console.log(membre)
-    //MILESTONE 2:
-    //Stampare le stesse informazioni su DOM sottoforma di stringhe
-    rowEl.innerHTML += `
+    rowEl.innerHTML += 
+            //MILESTONE 2:
+                //Stampare le stesse informazioni su DOM sottoforma di stringhe
+    /* `
     Nome:  ${membre.nome} <br>
     Ruolo: ${membre.ruolo} <br>
-    Foto:  ${membre.foto}`
+    Foto:  ${membre.foto}` */
+
+
+            //BONUS 1:
+                //Trasformare la stringa foto in una immagine effettiva
+            //BONUS 2:
+                //Organizzare i singoli membri in card/schede 
+`   <div class="col">
+        <div class="card">
+            <div class="card-header">
+                <h1>
+                    ${membre.nome}
+                </h1>
+            </div>
+            <div class="card-body">
+                <img src="./asset/img/${membre.foto}" alt="" class="">
+                <p>
+                    ${membre.ruolo}
+                </p>
+            </div>
+        </div>
+    </div>`
+
         
 }
